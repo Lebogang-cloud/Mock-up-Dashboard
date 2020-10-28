9bc88752653ac06c6b15ed940eb30a854f3c8f59
Lebo <mphagol@gmail.com>
2020-10-28 12:16:37 +0200

    content of cards

--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -6,34 +6,67 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyle = makeStyles({
+const useStyle = makeStyles((theme) => ({
     root: {
         minWidth: 275,
-        margin: "70px"
+        margin: "100px",
+        background: theme.background,
+        border: 0,
+        borderRadius:3,
+        boxShadow:theme.boxShadow,
+        color: 'white',
+        padding: '0 30px'
     },
-    bullet: {
-        display: 'inline-block',
-        margin: '0 2px',
-        transform: 'scale(0.8)'
-    },
+   
     title: {
         fontSize: 14
     },
     pos:{
         marginBottom: 12
+    },
+    heading:{
+        textAlign:'center',
+        fontSize:'25px',
+        fontVariant:'h1'
+        
     }
-});
+
+}));
 
 export default function Feedback(){
     const classes = useStyle();
     
-    return(
+    return(    
         <Card className={classes.root} variant="Outlined">
             <CardContent>
+                <Typography className={classes.heading} color="textPrimary">FEEDBACK</Typography>
                 <Typography className={classes.title} color="textSecondary" gutterBottom>
                     Topic: 
+                    <br></br>
+                    Card id:
                 </Typography>
-            </CardContent>
+                <Typography className={classes.title} color="primary">
+                    Introduction to Tilde
+                </Typography>
+                <br></br>
+                <Typography className={classes.pos} color="textSecondary" >
+                    Assignee:
+                </Typography>
+                <Typography className={classes.pos} color="textSecondary" >
+                    Reviewer:
+                </Typography>
+                </CardContent>
+                <CardActions>
+                <Button size="medium">VIEW CONTENT</Button>
+                <br/>
+                <br></br>
+                <Button size="medium">DETAILS</Button>
+                <br></br>
+                <Button size="medium">EDIT REVIEW</Button>
+            </CardActions>
+            
         </Card>
+        
     )
 }
+
